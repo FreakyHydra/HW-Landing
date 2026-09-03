533a8bc010685e26eac6e36e9f718cd65f55b4a6
Arrax Shadowfang <freakyhydra@gmail.com>
2026-09-03 22:56:50 +0200

    feat: wire response length controls

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ import { AppRouter } from './app/router'
 import { LocalCharacterRepository, LocalPersonaRepository, LocalWorldRepository } from './data/repositories'
 import { CanonicalPublicWorldRepository } from './data/canonical-public-worlds'
 import { installPlayerMessageColors } from './runtime/player-message-colors'
+import { installRoleplayLengthControls } from './runtime/roleplay-length-controls'
 
 const app = document.querySelector<HTMLDivElement>('#app')
 if (!app) throw new Error('Application root is missing')
 
 installPlayerMessageColors()
+installRoleplayLengthControls()
 
 const router = new AppRouter(app, {
   characters: new LocalCharacterRepository(),
